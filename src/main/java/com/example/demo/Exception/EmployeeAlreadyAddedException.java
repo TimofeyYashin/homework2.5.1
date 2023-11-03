9d8a99aee628ef2102c8bf21281efbccc2c68202
г <ashin.timoti2018@gmail.com>
2023-11-04 01:52:42 +0300

    homework2.5.1

---
+++ src/main/java/com/example/demo/Exception/EmployeeAlreadyAddedException.java
@@ -0,0 +1,11 @@
+package com.example.demo.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class EmployeeAlreadyAddedException extends  RuntimeException {
+    public EmployeeAlreadyAddedException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/demo/Exception/EmployeeStorageIsFullException.java
@@ -0,0 +1,11 @@
+package com.example.demo.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class EmployeeStorageIsFullException extends RuntimeException {
+    public EmployeeStorageIsFullException(String message) {
+        super(message);
+    }
+}
