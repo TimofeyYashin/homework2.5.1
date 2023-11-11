20846b4d3d16e3b6a88be2172bda06cd5bdd832b
г <ashin.timoti2018@gmail.com>
2023-11-11 13:32:30 +0300

    homework2.5.1

--- src/main/java/com/example/demo/Employee.java
+++ src/main/java/com/example/demo/Employee.java
@@ -26,7 +26,7 @@ public class Employee {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object  o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
